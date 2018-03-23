d6f483629dc058085076960867b4ef5b2124ddc9
zmengxia <1641721960@qq.com>
2018-03-23 10:03:23 +0800

    换成

--- temp/compiled/admin/user_tupu.htm.php
+++
@@ -1,8 +0,0 @@
-<!-- $Id: user_info.htm 16854 2009-12-07 06:20:09Z sxc_shop $ -->
-<?php echo $this->fetch('pageheader.htm'); ?>
-<?php echo $this->smarty_insert_scripts(array('files'=>'../js/transport.org.js,../js/region.js')); ?>
-
-<div style="width:10%;height:50px;margin:0 auto;border:1px solid red">haha</div>
-
-
-<?php echo $this->fetch('pagefooter.htm'); ?>
--- temp/query_caches/sqlcache_config_file_d8953938d2c270a91532917f8afbc890.php
+++ temp/query_caches/sqlcache_config_file_d8953938d2c270a91532917f8afbc890.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1521765823;
+$this->mysql_config_cache_file_time = 1521770390;
 $this->timeline = 0;
 $this->timezone = 0;
 $this->platform = 'OTHER';
