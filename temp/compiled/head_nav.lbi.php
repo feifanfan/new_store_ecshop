ad1ca47ab52ed3c9e71eda049b6504c47fde68bd
zmengxia <1641721960@qq.com>
2018-05-04 14:30:40 +0800

    日期汇总

--- temp/compiled/head_nav.lbi.php
+++ temp/compiled/head_nav.lbi.php
@@ -1,10 +1,10 @@
 <ul class="allMenu Left">
   <li><a class="index nav" href="/" title="首页">首页</a></li>
-  <?php $_from = $this->_var['navigator_list']['middle']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'nav_0_33850100_1525249072');$this->_foreach['nav_middle_list'] = array('total' => count($_from), 'iteration' => 0);
+  <?php $_from = $this->_var['navigator_list']['middle']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'nav_0_83010600_1525312711');$this->_foreach['nav_middle_list'] = array('total' => count($_from), 'iteration' => 0);
 if ($this->_foreach['nav_middle_list']['total'] > 0):
-    foreach ($_from AS $this->_var['nav_0_33850100_1525249072']):
+    foreach ($_from AS $this->_var['nav_0_83010600_1525312711']):
         $this->_foreach['nav_middle_list']['iteration']++;
 ?> 
-  <li><a href="<?php echo $this->_var['nav_0_33850100_1525249072']['url']; ?>"  class="nav <?php if ($this->_var['nav_0_33850100_1525249072']['active'] == 1): ?>current<?php endif; ?>" title="<?php echo $this->_var['nav_0_33850100_1525249072']['name']; ?>" <?php if ($this->_var['nav_0_33850100_1525249072']['opennew'] == 1): ?>target="_blank" <?php endif; ?>><?php echo $this->_var['nav_0_33850100_1525249072']['name']; ?></a></li>  
+  <li><a href="<?php echo $this->_var['nav_0_83010600_1525312711']['url']; ?>"  class="nav <?php if ($this->_var['nav_0_83010600_1525312711']['active'] == 1): ?>current<?php endif; ?>" title="<?php echo $this->_var['nav_0_83010600_1525312711']['name']; ?>" <?php if ($this->_var['nav_0_83010600_1525312711']['opennew'] == 1): ?>target="_blank" <?php endif; ?>><?php echo $this->_var['nav_0_83010600_1525312711']['name']; ?></a></li>  
   <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>
 </ul>
--- temp/query_caches/sqlcache_config_file_d8953938d2c270a91532917f8afbc890.php
+++ temp/query_caches/sqlcache_config_file_d8953938d2c270a91532917f8afbc890.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1525311596;
+$this->mysql_config_cache_file_time = 1525414732;
 $this->timeline = 0;
 $this->timezone = 0;
 $this->platform = 'OTHER';
